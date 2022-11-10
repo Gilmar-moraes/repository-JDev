0fc19637d4124f555456ad8e158bc28cf6084b72
Gilmar Moraes <gilmar14moraes@hotmail.com>
2022-11-10 10:36:46 -0300

    commit com a finalização parte 1 entrada de dados

--- primeiro-projeto-java/src/cursojava/execultavel/PrimeiraClasseJava.java
+++ primeiro-projeto-java/src/cursojava/execultavel/PrimeiraClasseJava.java
@@ -1,5 +1,7 @@
 package cursojava.execultavel;
 
+import javax.swing.JOptionPane;
+
 import cursojava.classes.Aluno;
 
 public class PrimeiraClasseJava {
@@ -8,12 +10,54 @@ public class PrimeiraClasseJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		/*Objeto temos um real na memoria*/
-		Aluno aluno1 = new Aluno();
+		/*
+		 * Entrada de dados pelo usuario
+		 */
+		String alunoNome = JOptionPane.showInputDialog("Qual o nome do aluno?");
+		String alunoIdade = JOptionPane.showInputDialog("Qual o idade do aluno?");
+		String alunoNascimento = JOptionPane.showInputDialog("Qual a data de Nascimento do aluno?");
+		String alunoRG = JOptionPane.showInputDialog("Qual o RG do aluno?");
+		String alunoCpf = JOptionPane.showInputDialog("Qual o CPF do aluno?");
+		String alunoMae = JOptionPane.showInputDialog("Qual o nome da Mãe do aluno?");
+		String alunoPai = JOptionPane.showInputDialog("Qual o nome do Pai do aluno?");
 		
-		/*Objeto ainda não existe na memoria*/
-		Aluno aluno2 = new Aluno();
-
+		
+		String dataMatriculaAluno = JOptionPane.showInputDialog("Qual a data de matricula do aluno?");
+		String serieAluno = JOptionPane.showInputDialog("Qual a serie do aluno?");
+		String nomeEscola = JOptionPane.showInputDialog("Qual o nome da escola do aluno?");
+		String alunoNota1 = JOptionPane.showInputDialog("Qual a primeira nota do aluno?");
+		String alunoNota2 = JOptionPane.showInputDialog("Qual a segunda nota do aluno?");
+		String alunoNota3 = JOptionPane.showInputDialog("Qual a terceira nota do aluno?");
+		String alunoNota4 = JOptionPane.showInputDialog("Qual a quarta nota do aluno?");
+		
+		/*
+		 * Aluno1 é uma referência para o objeto aluno
+		 * Objeto temos um real na memoria
+		 */
+		Aluno aluno1 = new Aluno();
+		aluno1.setNome(alunoNome);
+		aluno1.setIdade(Integer.valueOf(alunoIdade));
+		aluno1.setDataNascimento(alunoNascimento);
+		aluno1.setRegistroGeral(alunoRG);
+		aluno1.setNumeroCpf(alunoCpf);
+		aluno1.setNomeMae(alunoMae);
+		aluno1.setNomePai(alunoPai);
+		
+		aluno1.setDataMatricula(dataMatriculaAluno);
+		aluno1.setSerieMatriculado(serieAluno);
+		aluno1.setNomeEscola(nomeEscola);
+		aluno1.setNota1(Double.parseDouble(alunoNota1));
+		aluno1.setNota2(Double.parseDouble(alunoNota2));
+		aluno1.setNota3(Double.parseDouble(alunoNota3));
+		aluno1.setNota4(Double.parseDouble(alunoNota4));
+		
+		System.out.println("---------------------------------------");
+		System.out.println("Nome é = " + aluno1.getNome());
+		System.out.println("Idade é = " + aluno1.getIdade());
+		System.out.println("Nascimento é = " + aluno1.getDataNascimento());
+		System.out.println("Media das notas do aluno " + aluno1.getNome() + " é:" + aluno1.getMediaNotas());
+		System.out.println("Resultado: " + (aluno1.getAlunoAprovado() ? "Aprovado" : "Resprovado"));
+		System.out.println("---------------------------------------");
 	}
 
 }
