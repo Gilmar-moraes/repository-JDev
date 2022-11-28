9e584459ca3a17e62ac4a41b78f1ee497f6d276e
Gilmar Moraes <gilmar14moraes@hotmail.com>
2022-11-28 08:49:06 -0300

    Criando a lista dinâmica das disciplinas do aluno

--- primeiro-projeto-java/src/cursojava/execultavel/PrimeiraClasseJava.java
+++ primeiro-projeto-java/src/cursojava/execultavel/PrimeiraClasseJava.java
@@ -7,10 +7,10 @@ import cursojava.classes.Disciplina;
 
 public class PrimeiraClasseJava {
 
-	/*Main é um metodo auto executavel do java*/
+	/* Main é um metodo auto executavel do java */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		/*
 		 * Entrada de dados pelo usuario
 		 */
@@ -21,24 +21,13 @@ public class PrimeiraClasseJava {
 		String alunoCpf = JOptionPane.showInputDialog("Qual o CPF do aluno?");
 		String alunoMae = JOptionPane.showInputDialog("Qual o nome da Mãe do aluno?");
 		String alunoPai = JOptionPane.showInputDialog("Qual o nome do Pai do aluno?");
-		
-		
+
 		String dataMatriculaAluno = JOptionPane.showInputDialog("Qual a data de matricula do aluno?");
 		String serieAluno = JOptionPane.showInputDialog("Qual a serie do aluno?");
 		String nomeEscola = JOptionPane.showInputDialog("Qual o nome da escola do aluno?");
-		
-		String disciplina1 = JOptionPane.showInputDialog("Disciplina 1");
-		String alunoNota1 = JOptionPane.showInputDialog("Qual a primeira nota do aluno?");
-		String disciplina2 = JOptionPane.showInputDialog("Disciplina 2");
-		String alunoNota2 = JOptionPane.showInputDialog("Qual a segunda nota do aluno?");
-		String disciplina3 = JOptionPane.showInputDialog("Disciplina 3");
-		String alunoNota3 = JOptionPane.showInputDialog("Qual a terceira nota do aluno?");
-		String disciplina4 = JOptionPane.showInputDialog("Disciplina 4");
-		String alunoNota4 = JOptionPane.showInputDialog("Qual a quarta nota do aluno?");
-		
+
 		/*
-		 * Aluno1 é uma referência para o objeto aluno
-		 * Objeto temos um real na memoria
+		 * Aluno1 é uma referência para o objeto aluno Objeto temos um real na memoria
 		 */
 		Aluno aluno1 = new Aluno();
 		aluno1.setNome(alunoNome);
@@ -48,31 +37,23 @@ public class PrimeiraClasseJava {
 		aluno1.setNumeroCpf(alunoCpf);
 		aluno1.setNomeMae(alunoMae);
 		aluno1.setNomePai(alunoPai);
-		
+
 		aluno1.setDataMatricula(dataMatriculaAluno);
 		aluno1.setSerieMatriculado(serieAluno);
 		aluno1.setNomeEscola(nomeEscola);
-		
-		Disciplina disciplina10 = new Disciplina();
-		disciplina10.setDisciplina("java");
-		disciplina10.setNota(10);
-		aluno1.getDisciplina().add(disciplina10);
-		
-		Disciplina disciplina11 = new Disciplina();
-		disciplina11.setDisciplina("java ee");
-		disciplina11.setNota(10);
-		aluno1.getDisciplina().add(disciplina11);
-		
-		Disciplina disciplina12 = new Disciplina();
-		disciplina11.setDisciplina("java se");
-		disciplina11.setNota(12);
-		aluno1.getDisciplina().add(disciplina12);
-		
-		Disciplina disciplina13 = new Disciplina();
-		disciplina11.setDisciplina("java me");
-		disciplina11.setNota(13);
-		aluno1.getDisciplina().add(disciplina13);
-		
+
+		for (int pos = 1; pos <= 4; pos++) {
+
+			String nomeDisciplina = JOptionPane.showInputDialog("Nome da Disciplina" + pos + "?");
+			String notaDisciplina = JOptionPane.showInputDialog("Qual a nota da Disciplina:" + pos + "?");
+
+			Disciplina disciplina = new Disciplina();
+			disciplina.setDisciplina(nomeDisciplina);
+			disciplina.setNota(Double.valueOf(notaDisciplina));
+
+			aluno1.getDisciplina().add(disciplina);
+		}
+
 		System.out.println("---------------------------------------");
 		System.out.println("Nome é = " + aluno1.getNome());
 		System.out.println("Idade é = " + aluno1.getIdade());
